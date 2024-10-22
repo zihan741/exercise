0466e3479e756636e6ec843e7deb6b2ee23284ec
Zihan Hossain <157952853+zihan741@users.noreply.github.com>
2024-10-23 01:15:51 +0600

    area and perimeter of a rectangle

---
+++ Problem9.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float l,w;
+    cin>>l;
+    cin>>w;
+    float area=l*w;
+    float perimeter=2*(l+w);
+    cout<<area<<endl;
+    cout<<perimeter<<endl;
+}
