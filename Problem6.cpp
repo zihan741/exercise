e4ee898ff2627dd08fc8a49bf8f4140dd9c54362
Zihan Hossain <157952853+zihan741@users.noreply.github.com>
2024-10-23 01:03:38 +0600

    Swap two numbers

---
+++ Problem6.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,temp;
+    cin>>a;
+    cin>>b;
+   temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    
+}
