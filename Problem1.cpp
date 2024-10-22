8639001faf6f59d7d033f32d70aaad7b72a0532d
Zihan Hossain <157952853+zihan741@users.noreply.github.com>
2024-10-22 16:10:16 +0600

    Reverse a Number

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    int n,remainder,reverse=0;
+    cout<<"Enter Number for Reverse: ";
     cin>>n;
-  
-        if(n%2==0){
-            cout<<"Even";
-        }
-        else
-        cout<<"Odd";
-    
+   while(n>0){
+        remainder=n%10;
+        reverse=reverse*10+remainder;
+        n=n/10;
+    }
+    cout<<reverse;
 }
